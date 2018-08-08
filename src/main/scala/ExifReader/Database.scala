7ea11955acf81fd05c3c0fa2a0d36b13c3acb7e1
Jason Racey <jasonracey@gmail.com>
2018-08-08 08:44:56 -0700

    Databse use StringBuilder and some refactoring

--- src/main/scala/ExifReader/Database.scala
+++ src/main/scala/ExifReader/Database.scala
@@ -4,71 +4,81 @@ import java.sql._
 
 object Database
 {
-  // todo: use string builder?
-  private val createTableIfNotExists: String = "CREATE TABLE IF NOT EXISTS photograph (" +
-    "id INTEGER PRIMARY KEY, " +
-    "aperture REAL, " +
-    "cameraMake TEXT, " +
-    "cameraModel TEXT, " +
-    "colorSpace TEXT, " +
-    "createDate TEXT, " +
-    "directory TEXT NOT NULL, " +
-    "fileName TEXT NOT NULL, " +
-    "focalLength INTEGER, " +
-    "pixelHeight INTEGER, " +
-    "pixelWidth INTEGER, " +
-    "iso INTEGER, " +
-    "lens TEXT, " +
-    "megaPixels REAL, " +
-    "orientation TEXT, " +
-    "shutterSpeed REAL, " +
-    "UNIQUE (directory, fileName) ON CONFLICT ROLLBACK)"
+  val batchSize: Int = 100
 
-  // todo: use string builder?
-  private val insertPhotograph: String = s"INSERT INTO photograph (" +
-    "aperture, " +
-    "cameraMake, " +
-    "cameraModel, " +
-    "colorSpace, " +
-    "createDate, " +
-    "directory, " +
-    "fileName, " +
-    "focalLength, " +
-    "pixelHeight, " +
-    "pixelWidth, " +
-    "iso, " +
-    "lens, " +
-    "megaPixels, " +
-    "orientation, " +
-    s"shutterSpeed) VALUES"
+  private val createTableIfNotExistsSql: String = {
+    val initialCapacity: Int = 600
+
+    val sb = new StringBuilder(initialCapacity)
+
+    sb ++= "CREATE TABLE IF NOT EXISTS photograph ("
+    sb ++= "id INTEGER PRIMARY KEY, "
+    sb ++= "aperture REAL, "
+    sb ++= "cameraMake TEXT, "
+    sb ++= "cameraModel TEXT, "
+    sb ++= "colorSpace TEXT, "
+    sb ++= "createDate TEXT, "
+    sb ++= "directory TEXT NOT NULL, "
+    sb ++= "fileName TEXT NOT NULL, "
+    sb ++= "focalLength INTEGER, "
+    sb ++= "pixelHeight INTEGER, "
+    sb ++= "pixelWidth INTEGER, "
+    sb ++= "iso INTEGER, "
+    sb ++= "lens TEXT, "
+    sb ++= "megaPixels REAL, "
+    sb ++= "orientation TEXT, "
+    sb ++= "shutterSpeed REAL, "
+    sb ++= "UNIQUE (directory, fileName) ON CONFLICT ROLLBACK)"
+
+    sb.result
+  }
+
+  private val insertPhotographSnippet: String = {
+    val initialCapacity: Int = 350
+
+    val sb = new StringBuilder(initialCapacity)
+
+    sb ++= "INSERT INTO photograph ("
+    sb ++= "aperture, "
+    sb ++= "cameraMake, "
+    sb ++= "cameraModel, "
+    sb ++= "colorSpace, "
+    sb ++= "createDate, "
+    sb ++= "directory, "
+    sb ++= "fileName, "
+    sb ++= "focalLength, "
+    sb ++= "pixelHeight, "
+    sb ++= "pixelWidth, "
+    sb ++= "iso, "
+    sb ++= "lens, "
+    sb ++= "megaPixels, "
+    sb ++= "orientation, "
+    sb ++= "shutterSpeed) VALUES"
+
+    sb.result
+  }
 
   def insertPhotographs(photographs: Seq[Photograph]): Unit = {
-
     val sortedByPathAndName: Seq[Photograph] = photographs.sortBy{ photograph: Photograph =>
       (photograph.directory, photograph.fileName)
     }
 
-    // todo: what happens when orNull?
-    // todo: use string builder?
-    val valuesStatements: String = sortedByPathAndName.map{ photograph: Photograph =>
-      s"(${photograph.aperture.orNull}, " +
-      s"'${photograph.cameraMake.orNull}', " +
-      s"'${photograph.cameraModel.orNull}', " +
-      s"'${photograph.colorSpace.orNull}', " +
-      s"'${photograph.createDate.orNull}', " +
-      s"'${photograph.directory.getOrElse{ throw new Exception("directory must not be null") }}', " +
-      s"'${photograph.fileName.getOrElse{ throw new Exception("fileName must not be null") }}', " +
-      s"${photograph.focalLength.map{ _.mm }.orNull}, " +
-      s"${photograph.pixelHeight.orNull}, " +
-      s"${photograph.pixelWidth.orNull}, " +
-      s"${photograph.iso.orNull}, " +
-      s"'${photograph.lens.orNull}', " +
-      s"${photograph.megaPixels.orNull}, " +
-      s"'${photograph.orientation.orNull}', " +
-      s"${photograph.shutterSpeed.map{ _.seconds }.orNull})"
-    }.mkString(",")
+    val batches: Iterator[Seq[Photograph]] = sortedByPathAndName.grouped(batchSize)
 
-    val batchInsertPhotograph: String = s"$insertPhotograph $valuesStatements"
+    val totalBatchCount: Int = batches.size
+    var currentBatchCount: Int = 1
+
+    batches.foreach{ batch: Seq[Photograph] =>
+      insert(batch)
+      println(s"inserted batch $currentBatchCount of $totalBatchCount")
+      currentBatchCount += 1
+    }
+  }
+
+  private def insert(photographs: Seq[Photograph]): Unit = {
+    val valuesStatements: String = photographs.map{ makeValuesStatement }.mkString(",")
+
+    val insertPhotographSql: String = s"$insertPhotographSnippet $valuesStatements"
 
     // load the sqlite-JDBC driver using the current class loader
     Class.forName("org.sqlite.JDBC")
@@ -78,11 +88,10 @@ object Database
     try {
       connection = DriverManager.getConnection("jdbc:sqlite:photographs.db")
       val statement: Statement = connection.createStatement
-      // todo: can this timeout accommodate a 10K item batch?
       statement.setQueryTimeout(30) // seconds
-      statement.executeUpdate(createTableIfNotExists)
+      statement.executeUpdate(createTableIfNotExistsSql)
       // todo: indexes?
-      statement.executeUpdate(batchInsertPhotograph)
+      statement.executeUpdate(insertPhotographSql)
     } catch {
       // if the error message is "out of memory" it probably means no database file is found
       case e: SQLException => System.err.println(e.getMessage)
@@ -92,5 +101,31 @@ object Database
       // connection close failed
       case e: SQLException => System.err.println(e)
     }
+  }
+
+  private def makeValuesStatement(photograph: Photograph): String = {
+    val initialCapacity: Int = 500
+
+    val sb = new StringBuilder(initialCapacity)
+
+    // todo: what happens when orNull?
+    sb ++= s"(${photograph.aperture.orNull}, "
+    sb ++= s"'${photograph.cameraMake.orNull}', "
+    sb ++= s"'${photograph.cameraModel.orNull}', "
+    sb ++= s"'${photograph.colorSpace.orNull}', "
+    sb ++= s"'${photograph.createDate.orNull}', "
+    // todo: weird to be enforcing this here?
+    sb ++= s"'${photograph.directory.getOrElse{ throw new Exception("directory must not be null") }}', "
+    sb ++= s"'${photograph.fileName.getOrElse{ throw new Exception("fileName must not be null") }}', "
+    sb ++= s"${photograph.focalLength.map{ _.mm }.orNull}, "
+    sb ++= s"${photograph.pixelHeight.orNull}, "
+    sb ++= s"${photograph.pixelWidth.orNull}, "
+    sb ++= s"${photograph.iso.orNull}, "
+    sb ++= s"'${photograph.lens.orNull}', "
+    sb ++= s"${photograph.megaPixels.orNull}, "
+    sb ++= s"'${photograph.orientation.orNull}', "
+    sb ++= s"${photograph.shutterSpeed.map{ _.seconds }.orNull})"
+
+    sb.result
   }
 }
