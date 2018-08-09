975041f9f3b461468c8694661950f355861b8582
Jason Racey <jasonracey@gmail.com>
2018-08-08 21:15:12 -0700

    Added ability to debug app when run from shell script

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Uncomment to enable debugging
+# export JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+
 sbt "runMain ExifReader.ExifReaderApp $@"
