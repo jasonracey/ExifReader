c7ab4671cf67e45852848fbb2991107c06ae1ad9
Jason Racey <jasonracey@gmail.com>
2018-08-08 21:58:21 -0700

    Data type was switched to float

--- src/test/scala/ExifReader/FocalLengthTests.scala
+++ src/test/scala/ExifReader/FocalLengthTests.scala
@@ -4,37 +4,37 @@ import org.scalatest.FunSuite
 
 class FocalLengthTests  extends FunSuite {
   test("can parse expected format"){
-    val result: Option[FocalLength] = FocalLength.get(Some("50 mm"))
+    val result: Option[FocalLength] = FocalLength.get(Some("50.0 mm"))
     assert(result.isDefined)
-    assert(result.get.mm == 50)
+    assert(result.get.mm == 50.0f)
   }
 
   test("unit is case-insensitive"){
-    val result: Option[FocalLength] = FocalLength.get(Some("50 MM"))
+    val result: Option[FocalLength] = FocalLength.get(Some("50.0 MM"))
     assert(result.isDefined)
-    assert(result.get.mm == 50)
+    assert(result.get.mm == 50.0f)
   }
 
   test("can parse format without space before unit"){
-    val result: Option[FocalLength] = FocalLength.get(Some("50mm"))
+    val result: Option[FocalLength] = FocalLength.get(Some("50.0mm"))
     assert(result.isDefined)
-    assert(result.get.mm == 50)
+    assert(result.get.mm == 50.0f)
   }
 
   test("can parse format without unit"){
-    val result: Option[FocalLength] = FocalLength.get(Some("50"))
+    val result: Option[FocalLength] = FocalLength.get(Some("50.0"))
     assert(result.isDefined)
-    assert(result.get.mm == 50)
+    assert(result.get.mm == 50.0f)
   }
 
   test("input is trimmed before parsing"){
-    val result: Option[FocalLength] = FocalLength.get(Some("   50     mm    "))
+    val result: Option[FocalLength] = FocalLength.get(Some("   50.0     mm    "))
     assert(result.isDefined)
-    assert(result.get.mm == 50)
+    assert(result.get.mm == 50.0f)
   }
 
   test("unexpected format returns None"){
-    val result: Option[FocalLength] = FocalLength.get(Some("50 millimeters"))
+    val result: Option[FocalLength] = FocalLength.get(Some("50.0 millimeters"))
     assert(result.isEmpty)
   }
 }
